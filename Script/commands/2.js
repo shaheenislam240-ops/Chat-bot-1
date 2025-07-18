a87b2a2bc2c3bbd616bfe3eeb72f1ba6324903b6
rX Abdullah <rxmaria761@gmail.com>
2025-07-18 21:55:35 +0600

    Update and rename mohanobi.js to 2.js

--- Script/commands/2.js
+++ Script/commands/2.js
@@ -8,13 +8,13 @@ const link = [
 ];
 
 module.exports.config = {
- name: "😭",
+ name: "😔",
  version: "1.0.0",
  hasPermssion: 0,
  credits: "Islamick Chat",
  description: "auto reply to salam",
  commandCategory: "noprefix",
- usages: "😭",
+ usages: "😔",
  cooldowns: 5,
  dependencies: {
  "request":"",
@@ -26,7 +26,7 @@ module.exports.config = {
 module.exports.handleEvent = async ({ api, event, Threads }) => {
  const content = event.body ? event.body : '';
  const body = content.toLowerCase();
- if (body.startsWith("🥺")) {
+ if (body.startsWith("😞")) {
  const rahad = [
  "╭•┄┅════❁🌺❁════┅┄•╮\n \n আমি বলবো কেমন করে আমার শরিলের লোম দারিয়ে যায়-!!🥺\n\n╰•┄┅════❁🌺❁════┅┄•╯",
  "╭•┄┅════❁🌺❁════┅┄•╮\n\nআমি বলবো কেমন করে আমার শরিলের লোম দারিয়ে যায়-!!🥺\n\n╰•┄┅════❁🌺❁════┅┄•╯"
@@ -61,9 +61,9 @@ module.exports.languages = {
 module.exports.run = async ({ api, event, Threads, getText }) => {
  const { threadID, messageID } = event;
  let data = (await Threads.getData(threadID)).data;
- if (typeof data["😭"] === "undefined" || data["😭"]) data["😭"] = false;
- else data["😭"] = true;
+ if (typeof data["😞"] === "undefined" || data["😭"]) data["😭"] = false;
+ else data["😞"] = true;
  await Threads.setData(threadID, { data });
  global.data.threadData.set(threadID, data);
- api.sendMessage(`${(data["😭"]) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+ api.sendMessage(`${(data["😞"]) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
 };
