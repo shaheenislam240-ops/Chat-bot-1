e91a7e59cf17fd921b91e6b9af792413dd8b5983
rX Abdullah <rxmaria761@gmail.com>
2025-07-25 19:43:19 +0600

    Update auto.js

--- Script/commands/auto.js
+++ Script/commands/auto.js
@@ -18,18 +18,18 @@ handleEvent: async function ({ api, event, args }) {
  const body = content.toLowerCase();
  const { alldown } = require("shaon-videos-downloader")
  if (body.startsWith("https://")) {
- api.setMessageReaction("🐥", event.messageID, (err) => {}, true);
+ api.setMessageReaction("⚠️", event.messageID, (err) => {}, true);
 const data = await alldown(content);
  console.log(data)
  let Shaon = data.url;
- api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+ api.setMessageReaction("☢️", event.messageID, (err) => {}, true);
  const video = (await axios.get(Shaon, {
  responseType: "arraybuffer",
  })).data;
  fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
 
  return api.sendMessage({
- body: `rX Chat bot||ᴍᴀʀɪᴀ 
+ body: `🔥 rX 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁 | ᴍᴀʀɪᴀ 
 📥⚡𝗔𝘂𝘁𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿⚡📂
 🎬 𝐄𝐧𝐣𝐨𝐲 𝐭𝐡𝐞 𝐕𝐢𝐝𝐞𝐨 🎀`,
  attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
