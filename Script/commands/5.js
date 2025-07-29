4e6effcddfad44adcf5f3cd6459d852eae0ee50e
rX Abdullah <rxmaria761@gmail.com>
2025-07-30 00:37:23 +0600

    Update 5.js

--- Script/commands/5.js
+++ Script/commands/5.js
@@ -3,32 +3,37 @@ const fs = require("fs");
 const request = require("request");
 
 const link = [
- "https://i.imgur.com/8tJ70qr.mp4",
-
+  "https://i.imgur.com/8tJ70qr.mp4",
 ];
 
 module.exports.config = {
- name: "👿",
- version: "1.0.0",
- hasPermssion: 0,
- credits: "Islamick Chat",
- description: "auto reply to salam",
- commandCategory: "noprefix",
- usages: "👿",
- cooldowns: 5,
- dependencies: {
- "request":"",
- "fs-extra":"",
- "axios":""
- }
+  name: "👿",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "Islamick Chat",
+  description: "auto reply to 👿 trigger",
+  commandCategory: "noprefix",
+  usages: "👿",
+  cooldowns: 5,
+  dependencies: {
+    "request": "",
+    "fs-extra": "",
+    "axios": ""
+  }
 };
 
 module.exports.handleEvent = async ({ api, event, Threads }) => {
- const content = event.body ? event.body : '';
- const body = content.toLowerCase();
- if (body.startsWith(👿")) {
- const rahad = [
- "★彡🌙⛧∘₊˚⋆ 𝑨𝑩𝑫𝑼𝑳𝑳𝑨𝐇 𝑴𝑶𝑫𝑬 ∘₊˚⋆⛧🌙彡★
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+
+  // ✅ Check if 👿 feature is off
+  const threadData = await Threads.getData(event.threadID);
+  const data = threadData.data || {};
+  if (data["👿"] === false) return;
+
+  if (body.startsWith("👿")) {
+    const rahad = [
+      `★彡🌙⛧∘₊˚⋆ 𝑨𝑩𝑫𝑼𝑳𝐋𝐀𝐇 𝑴𝑶𝑫𝑬 ∘₊˚⋆⛧🌙彡★
 
 ⚡ ᴘᴏᴡᴇʀ ʟᴇᴠᴇʟ: 9999%
 
@@ -36,41 +41,41 @@ module.exports.handleEvent = async ({ api, event, Threads }) => {
 🌟 Sᴜᴘᴇʀ Sᴀɪʏᴀɴ Mᴏᴅᴇ Aᴄᴛɪᴠᴀᴛᴇᴅ 🌟
 ｡･ﾟﾟ･　★　･ﾟﾟ･｡
 
-༺🌙 𝐑𝐗 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇 𝐁𝐎𝐒𝐒 𝐎𝐅 𝐁𝐎𝐒𝐒𝐄𝐒 🌙༻"
+༺🌙 𝐑𝐗 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇 𝐁𝐎𝐒𝐒 𝐎𝐅 𝐁𝐎𝐒𝐒𝐄𝐒 🌙༻`
+    ];
 
- ];
- const rahad2 = rahad[Math.floor(Math.random() * rahad.length)];
+    const rahad2 = rahad[Math.floor(Math.random() * rahad.length)];
 
- const callback = () => api.sendMessage({
- body: `${rahad2}`,
- attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")
- }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+    const callback = () => api.sendMessage({
+      body: `${rahad2}`,
+      attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
 
- const requestStream = request(encodeURI(link[Math.floor(Math.random() * link.length)]));
- requestStream.pipe(fs.createWriteStream(__dirname + "/cache/2024.mp4")).on("close", () => callback());
- return requestStream;
- }
+    const requestStream = request(encodeURI(link[Math.floor(Math.random() * link.length)]));
+    requestStream.pipe(fs.createWriteStream(__dirname + "/cache/2024.mp4")).on("close", callback);
+    return requestStream;
+  }
 };
 
 module.exports.languages = {
- "vi": {
- "on": "Dùng sai cách rồi lêu lêu",
- "off": "sv ngu, đã bão dùng sai cách",
- "successText": `🧠`,
- },
- "en": {
- "on": "on",
- "off": "off",
- "successText": "success!",
- }
+  "vi": {
+    "on": "Dùng sai cách rồi lêu lêu",
+    "off": "sv ngu, đã bão dùng sai cách",
+    "successText": `🧠`,
+  },
+  "en": {
+    "on": "on",
+    "off": "off",
+    "successText": "success!",
+  }
 };
 
 module.exports.run = async ({ api, event, Threads, getText }) => {
- const { threadID, messageID } = event;
- let data = (await Threads.getData(threadID)).data;
- if (typeof data["👿"] === "undefined" || data["👿"]) data["👿"] = false;
- else data["👿"] = true;
- await Threads.setData(threadID, { data });
- global.data.threadData.set(threadID, data);
- api.sendMessage(`${(data["👿"]) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+  const { threadID, messageID } = event;
+  let data = (await Threads.getData(threadID)).data || {};
+  if (typeof data["👿"] === "undefined" || data["👿"] === true) data["👿"] = false;
+  else data["👿"] = true;
+  await Threads.setData(threadID, { data });
+  global.data.threadData.set(threadID, data);
+  api.sendMessage(`${(data["👿"]) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
 };
