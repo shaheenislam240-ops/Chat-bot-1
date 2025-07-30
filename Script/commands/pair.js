cce71ca31b1040dfb395a904a29a2d003b891f2a
rX Abdullah <rxmaria761@gmail.com>
2025-07-31 01:59:56 +0600

    Update pair.js

--- Script/commands/pair.js
+++ Script/commands/pair.js
@@ -44,8 +44,8 @@ async function makeImage({ one, two }) {
  let circleOne = await jimp.read(await circle(avatarOne));
 let circleTwo = await jimp.read(await circle(avatarTwo));
 pairing_img
- .composite(circleOne.resize(135, 135), 93, 57)
- .composite(circleTwo.resize(135, 135), 503, 205);
+ .composite(circleOne.resize(137, 137), 95, 57)
+ .composite(circleTwo.resize(137, 137), 505, 205);
 
  let raw = await pairing_img.getBufferAsync("image/png");
 
