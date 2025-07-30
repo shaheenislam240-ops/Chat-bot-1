869e448ba4bc8afb145a2735610e6aa0148ff82b
rX Abdullah <rxmaria761@gmail.com>
2025-07-31 02:44:10 +0600

    Update pair3.js

--- Script/commands/pair3.js
+++ Script/commands/pair3.js
@@ -18,7 +18,7 @@ module.exports.onLoad = async () => {
   const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
   const { downloadFile } = global.utils;
   const dirMaterial = __dirname + `/cache/canvas/`;
-  const path = resolve(__dirname, 'cache/canvas', 'pairing.png');
+  const path = resolve(__dirname, 'cache/canvas', 'maria.png');
   if (!existsSync(dirMaterial)) mkdirSync(dirMaterial, { recursive: true });
   if (!existsSync(path)) await downloadFile("https://i.postimg.cc/TPKqsZ0L/r07qxo-R-Download.jpg", path);
 };
@@ -30,7 +30,7 @@ async function makeImage({ one, two }) {
   const jimp = global.nodemodule["jimp"];
   const __root = path.resolve(__dirname, "cache", "canvas");
 
-  let pairing_img = await jimp.read(__root + "/pairing.png");
+  let pairing_img = await jimp.read(__root + "/maria.png");
   let pathImg = __root + `/pairing_${one}_${two}.png`;
   let avatarOne = __root + `/avt_${one}.png`;
   let avatarTwo = __root + `/avt_${two}.png`;
