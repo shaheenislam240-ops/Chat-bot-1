813d1d0c9d8f4297a85704836a7d5744d3a208a0
rX Abdullah <rxmaria761@gmail.com>
2025-08-07 01:28:01 +0600

    Update adminupdate.js

--- Script/events/adminupdate.js
+++ Script/events/adminupdate.js
@@ -11,8 +11,10 @@ module.exports.config = {
 
 module.exports.run = async function ({ event, api, Threads, Users }) {
 	const fs = require("fs");
+	const moment = require("moment-timezone");
 	var iconPath = __dirname + "/emoji.json";
 	if (!fs.existsSync(iconPath)) fs.writeFileSync(iconPath, JSON.stringify({}));
+    
     const { threadID, logMessageType, logMessageData } = event;
     const { setData, getData } = Threads;
 
@@ -21,29 +23,51 @@ module.exports.run = async function ({ event, api, Threads, Users }) {
 
     try {
         let dataThread = (await getData(threadID)).threadInfo;
+
         switch (logMessageType) {
             case "log:thread-admins": {
+                const timeNow = moment.tz("Asia/Dhaka").format("dddd, h:mm A");
+
                 if (logMessageData.ADMIN_EVENT == "add_admin") {
                     dataThread.adminIDs.push({ id: logMessageData.TARGET_ID });
                     if (global.configModule[this.config.name].sendNoti) {
-                        api.sendMessage(`»» NOTICE «« Update user ${logMessageData.TARGET_ID} New Admin Added`, threadID);
+                        let addedBy = await Users.getNameUser(event.author);
+                        let newAdmin = await Users.getNameUser(logMessageData.TARGET_ID);
+
+                        const msg = `**[ 👑 ADMIN ADDED ]**
+・By         : ${addedBy}
+・Made Admin : ${newAdmin}
+・Time       : ${timeNow}`;
+
+                        api.sendMessage(msg, threadID);
                     }
                 } else if (logMessageData.ADMIN_EVENT == "remove_admin") {
                     dataThread.adminIDs = dataThread.adminIDs.filter(item => item.id != logMessageData.TARGET_ID);
                     if (global.configModule[this.config.name].sendNoti) {
-                        api.sendMessage(`»» NOTICE «« Update user ${logMessageData.TARGET_ID} Admin Removed`, threadID);
+                        let removedBy = await Users.getNameUser(event.author);
+                        let removedAdmin = await Users.getNameUser(logMessageData.TARGET_ID);
+
+                        const msg = `**[ 👑 ADMIN REMOVED ]**
+・By         : ${removedBy}
+・Removed    : ${removedAdmin}
+・Time       : ${timeNow}`;
+
+                        api.sendMessage(msg, threadID);
                     }
                 }
                 break;
             }
+
             case "log:user-nickname":
             case "log:thread-call":
             case "log:thread-color":
             case "log:thread-icon":
             case "log:thread-name": {
+                // ei khane future custom notifications add korte paro
                 break;
             }
         }
+
         await setData(threadID, { threadInfo: dataThread });
     } catch (e) {
         console.log(e);
