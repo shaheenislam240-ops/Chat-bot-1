4b97f896906d8f668c8dfe24268cbb973f3b6eb4
rX Abdullah <rxmaria761@gmail.com>
2025-08-06 09:23:37 +0600

    Update Rxabdullah.js

--- Rxabdullah.js
+++ Rxabdullah.js
@@ -38,7 +38,7 @@ global.countRestart = global.countRestart || 0;
 function startBot(message) {
     if (message) logger(message, "[ Starting ]");
 
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Abdullah.js"], {
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Cyber.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
