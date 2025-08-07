017bebee47e17aced9715e4860739d6335360fa4
rX Abdullah <rxmaria761@gmail.com>
2025-08-07 13:51:02 +0600

    Update and rename uptime3.js to upt.js

--- Script/commands/upt.js
+++ Script/commands/upt.js
@@ -5,7 +5,7 @@ const startTime = new Date(); // Bot start time
 
 module.exports = {
   config: {
-    name: "time",
+    name: "upt",
     version: "1.0.0",
     hasPermssion: 0,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭 ⚡ Updated with ADK by RX",
