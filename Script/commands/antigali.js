fef9237c11f508ef6861f89f41063443a8ef7350
rX Abdullah <rxmaria761@gmail.com>
2025-08-10 00:29:19 +0600

    Update antigali.js

--- Script/commands/antigali.js
+++ Script/commands/antigali.js
@@ -1,20 +1,19 @@
 let antiGaliStatus = false; // Default OFF
 
 const badWords = [
-  "হারামি", "হারাম", "কুত্তা", "কুত্তির বাচ্চা", "কুত্তার বাচ্চা", 
+  "কুত্তার বাচ্চা", 
   "মাগী", "মাগীচোদ", "চোদা", "চুদ", "চুদা", "চুদামারান", 
   "চুদির", "চুত", "চুদি", "চুতমারানি", "চুদের বাচ্চা", 
-  "বাল", "বালের", "বালের কাজ", "বালের ছেলে", "বালছাল", 
+  "Shawya", "বালের", "বালের ছেলে", "বালছাল", 
   "বালছাল কথা", "মাগীর ছেলে", "রান্ডি", "রান্দি", 
   "রান্দির ছেলে", "বেশ্যা", "বেশ্যাপনা", 
-  "তোর মায়ের", "তোর মায়েরে", "তোর বাপের", "তোর বাবার", 
-  "তোকে চুদি", "তুই হারামি", "তুই কুত্তা", "তুই চুদ",
+  "Khanki", "mgi", "তোকে চুদি", "তুই চুদ",
   "fuck", "f***", "f*ck", "fu*k", "fuk", "fking", "f***ing", "fucking", 
   "motherfucker", "mf", "mfer", "motherfu**er", "mthrfckr", 
   "bitch", "b!tch", "biatch", "slut", "whore", "bastard", 
   "asshole", "a$$hole", "a**hole", "dick", "d!ck", "cock", 
   "prick", "pussy", "pu$$y", "cunt", "fag", "faggot", "retard",
-  "harami", "kutte", "kutti", "magi", "magir", "magirchele", 
+  "magi", "magir", "magirchele", 
   "rand", "randir", "randirchele", "baler", "bal", "balsal", 
   "chuda", "chud", "chudir", "chut", "chudi", "chutmarani", 
   "tor mayer", "tor baper", "tui harami", "toke chudi", "chod"
