28620dc1dd50a9b9bed0994d7ff0a6e5febd291c
rX Abdullah <rxmaria761@gmail.com>
2025-08-13 20:41:36 +0600

    Update 0.js

--- Script/commands/0.js
+++ Script/commands/0.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "mm",
     version: "1.0.1",
 	hasPermssion: 2,
-	credits: "rX, // 
+	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭, // 
 	description: "Don't Change Credits",
 	commandCategory: "no prefix",
 	usages: "xxx",
