ffa75809f4a4e314919dd213c18326c724dda690
rX Abdullah <rxmaria761@gmail.com>
2025-08-15 22:07:39 +0600

    Update hack.js

--- Script/commands/hack.js
+++ Script/commands/hack.js
@@ -7,97 +7,111 @@ module.exports.config = {
   commandCategory: "hack",
   usages: "@mention",
   dependencies: {
-        "axios": "",
-        "fs-extra": ""
+    "axios": "",
+    "fs-extra": ""
   },
   cooldowns: 0
 };
 
 module.exports.wrapText = (ctx, name, maxWidth) => {
-	return new Promise(resolve => {
-		if (ctx.measureText(name).width < maxWidth) return resolve([name]);
-		if (ctx.measureText('W').width > maxWidth) return resolve(null);
-		const words = name.split(' ');
-		const lines = [];
-		let line = '';
-		while (words.length > 0) {
-			let split = false;
-			while (ctx.measureText(words[0]).width >= maxWidth) {
-				const temp = words[0];
-				words[0] = temp.slice(0, -1);
-				if (split) words[1] = `${temp.slice(-1)}${words[1]}`;
-				else {
-					split = true;
-					words.splice(1, 0, temp.slice(-1));
-				}
-			}
-			if (ctx.measureText(`${line}${words[0]}`).width < maxWidth) line += `${words.shift()} `;
-			else {
-				lines.push(line.trim());
-				line = '';
-			}
-			if (words.length === 0) lines.push(line.trim());
-		}
-		return resolve(lines);
-	});
-} 
+  return new Promise(resolve => {
+    if (ctx.measureText(name).width < maxWidth) return resolve([name]);
+    if (ctx.measureText("W").width > maxWidth) return resolve(null);
+    const words = name.split(" ");
+    const lines = [];
+    let line = "";
+    while (words.length > 0) {
+      let split = false;
+      while (ctx.measureText(words[0]).width >= maxWidth) {
+        const temp = words[0];
+        words[0] = temp.slice(0, -1);
+        if (split) words[1] = `${temp.slice(-1)}${words[1]}`;
+        else {
+          split = true;
+          words.splice(1, 0, temp.slice(-1));
+        }
+      }
+      if (ctx.measureText(`${line}${words[0]}`).width < maxWidth) line += `${words.shift()} `;
+      else {
+        lines.push(line.trim());
+        line = "";
+      }
+      if (words.length === 0) lines.push(line.trim());
+    }
+    return resolve(lines);
+  });
+};
 
-module.exports.run = async function ({ args, Users, Threads, api, event, Currencies }) {
+module.exports.run = async function ({ args, Users, api, event }) {
   const { loadImage, createCanvas } = require("canvas");
-  const fs = global.nodemodule["fs-extra"];
-  const axios = global.nodemodule["axios"];
+  const fs = require("fs-extra");
+  const axios = require("axios");
+
   let pathImg = __dirname + "/cache/background.png";
   let pathAvt1 = __dirname + "/cache/Avtmot.png";
-  
-  
+
+  // Get target ID & name
   var id = Object.keys(event.mentions)[0] || event.senderID;
   var name = await Users.getNameUser(id);
-  var ThreadInfo = await api.getThreadInfo(event.threadID);
-  
-  var background = [
 
+  // Background list
+  var background = [
     "https://i.imghippo.com/files/yYX4554yLo.jpeg"
-];
+  ];
   var rd = background[Math.floor(Math.random() * background.length)];
-  
+
+  // Get avatar
   let getAvtmot = (
     await axios.get(
-      `https://graph.facebook.com/${id}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662,
+      "https://graph.facebook.com/" + id + "/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662",
       { responseType: "arraybuffer" }
     )
   ).data;
-  fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
+  fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot));
 
+  // Get background
   let getbackground = (
-    await axios.get(`${rd}`, {
-      responseType: "arraybuffer",
-    })
+    await axios.get(rd, { responseType: "arraybuffer" })
   ).data;
-  fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
+  fs.writeFileSync(pathImg, Buffer.from(getbackground));
 
+  // Load images
   let baseImage = await loadImage(pathImg);
   let baseAvt1 = await loadImage(pathAvt1);
- 
+
+  // Create canvas
   let canvas = createCanvas(baseImage.width, baseImage.height);
   let ctx = canvas.getContext("2d");
+
+  // Draw background
   ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-    ctx.font = "400 23px Arial";
-	  ctx.fillStyle = "#1878F3";
-	  ctx.textAlign = "start";
-	  
-	  
-	  const lines = await this.wrapText(ctx, name, 1160);
-	  ctx.fillText(lines.join('\n'), 200,497);//comment
-	  ctx.beginPath();
 
+  // Text styling
+  ctx.font = "400 23px Arial";
+  ctx.fillStyle = "#1878F3";
+  ctx.textAlign = "start";
 
+  // Wrap text
+  const lines = await this.wrapText(ctx, name, 1160);
+  lines.forEach((line, i) => {
+    ctx.fillText(line, 200, 497 + i * 25); // line spacing
+  });
+
+  // Draw avatar
   ctx.drawImage(baseAvt1, 83, 437, 100, 101);
-  
+
+  // Save final image
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
+
+  // Remove temp avatar
   fs.removeSync(pathAvt1);
-  return api.sendMessage({ body: ` `, attachment: fs.createReadStream(pathImg) },
-      event.threadID,
-      () => fs.unlinkSync(pathImg),
-      event.messageID);
-    }
+
+  // Send image
+  return api.sendMessage(
+    { body: " ", attachment: fs.createReadStream(pathImg) },
+    event.threadID,
+    () => fs.unlinkSync(pathImg),
+    event.messageID
+  );
+};
