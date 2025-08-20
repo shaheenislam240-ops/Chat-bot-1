e562166991972dcd72871f6a19cd9de46364f785
rX Abdullah <rxmaria761@gmail.com>
2025-08-20 11:35:35 +0600

    Update unsend.js

--- Script/commands/unsend.js
+++ Script/commands/unsend.js
@@ -1,11 +1,11 @@
 module.exports.config = {
 	name: "unsend",
-	version: "1.0.1",
+	version: "1.0.2",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Gỡ tin nhắn của bot",
+	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭 + Modified by CYBER",
+	description: "Unsend bot messages by reply or angry reaction",
 	commandCategory: "system",
-	usages: "unsend",
+	usages: "unsend (reply bot msg) OR react 😡/🤬",
 	cooldowns: 0
 };
 
@@ -18,10 +18,27 @@ module.exports.languages = {
 		"returnCant": "Kisi Aur Ka Msg M Kese Unsend Karu.",
 		"missingReply": "Mere Jis Msg ko Unsend Karna Hai Usme Reply Karke Likkho."
 	}
-}
+};
 
 module.exports.run = function({ api, event, getText }) {
-	if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
-	if (event.type != "message_reply") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
+	if (event.type != "message_reply") 
+		return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
+	if (event.messageReply.senderID != api.getCurrentUserID()) 
+		return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
+	
 	return api.unsendMessage(event.messageReply.messageID);
+};
+
+// --- New feature: unsend if react 😡 or 🤬 ---
+module.exports.handleEvent = function({ api, event }) {
+	if (event.type === "message_reaction") {
+		// only unsend if react 😡 or 🤬 and message belongs to bot
+		if ((event.reaction === "😡" || event.reaction === "🤬") && event.senderID !== api.getCurrentUserID()) {
+			api.getMessageInfo(event.threadID, event.messageID, (err, info) => {
+				if (!err && info.message && info.message.senderID === api.getCurrentUserID()) {
+					api.unsendMessage(event.messageID);
+				}
+			});
+		}
 	}
+};
