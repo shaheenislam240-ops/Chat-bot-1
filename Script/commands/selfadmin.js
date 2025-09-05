671f20c563bc536133c0d85b6ee11d4119cfed1d
rX Abdullah <rxmaria761@gmail.com>
2025-09-05 13:52:04 +0600

    Update selfadmin.js

--- Script/commands/selfadmin.js
+++ Script/commands/selfadmin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
  }
  
  case "god": {
- const god = ["100000478146113"];
+ const god = ["61579782879961"];
  if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
  
 
