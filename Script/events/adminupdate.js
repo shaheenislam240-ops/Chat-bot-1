ecc05f5f41f2269ca8b1c710d9c0acb154a8d72f
rX Abdullah <rxmaria761@gmail.com>
2025-09-24 08:24:01 +0600

    Update adminupdate.js

--- Script/events/adminupdate.js
+++ Script/events/adminupdate.js
@@ -26,14 +26,14 @@ module.exports.run = async function ({ event, api, Threads, Users }) {
 
         // Bold font converter
         const toBold = (text) => {
-            const boldMap = {
-                "A":"𝗔","B":"𝗕","C":"𝗖","D":"𝗗","E":"𝗘","F":"𝗙","G":"𝗚","H":"𝗛","I":"𝗜","J":"𝗝","K":"𝗞","L":"𝗟","M":"𝗠",
-                "N":"𝗡","O":"𝗢","P":"𝗣","Q":"𝗤","R":"𝗥","S":"𝗦","T":"𝗧","U":"𝗨","V":"𝗩","W":"𝗪","X":"𝗫","Y":"𝗬","Z":"𝗭",
-                "a":"𝗮","b":"𝗯","c":"𝗰","d":"𝗱","e":"𝗲","f":"𝗳","g":"𝗴","h":"𝗵","i":"𝗶","j":"𝗷","k":"𝗸","l":"𝗹","m":"𝗺",
-                "n":"𝗻","o":"𝗼","p":"𝗽","q":"𝗾","r":"𝗿","s":"𝘀","t":"𝘁","u":"𝘂","v":"𝘃","w":"𝘄","x":"𝘅","y":"𝘆","z":"𝘇",
-                "0":"𝟬","1":"𝟭","2":"𝟮","3":"𝟯","4":"𝟰","5":"𝟱","6":"𝟲","7":"𝟳","8":"𝟴","9":"𝟵",
-                ":":":","-":"-"," ":" "
-            };
+        const boldMap = {
+    "A":"𝐀","B":"𝐁","C":"𝐂","D":"𝐃","E":"𝐄","F":"𝐅","G":"𝐆","H":"𝐇","I":"𝐈","J":"𝐉","K":"𝐊","L":"𝐋","M":"𝐌",
+    "N":"𝐍","O":"𝐎","P":"𝐏","Q":"𝐐","R":"𝐑","S":"𝐒","T":"𝐓","U":"𝐔","V":"𝐕","W":"𝐖","X":"𝐗","Y":"𝐘","Z":"𝐙",
+    "a":"𝐚","b":"𝐛","c":"𝐜","d":"𝐝","e":"𝐞","f":"𝐟","g":"𝐠","h":"𝐡","i":"𝐢","j":"𝐣","k":"𝐤","l":"𝐥","m":"𝐦",
+    "n":"𝐧","o":"𝐨","p":"𝐩","q":"𝐪","r":"𝐫","s":"𝐬","t":"𝐭","u":"𝐮","v":"𝐯","w":"𝐰","x":"𝐱","y":"𝐲","z":"𝐳",
+    "0":"𝟎","1":"𝟏","2":"𝟐","3":"𝟑","4":"𝟒","5":"𝟓","6":"𝟔","7":"𝟕","8":"𝟖","9":"𝟗",
+    ":":":","-":"-"," ":" "
+     };
             return text.split("").map(c => boldMap[c] || c).join("");
         }
 
@@ -48,9 +48,9 @@ module.exports.run = async function ({ event, api, Threads, Users }) {
                         let newAdmin = await Users.getNameUser(logMessageData.TARGET_ID);
 
                         const msg = `[ ADMIN ADDED ]
-・${toBold("By")}        : ${toBold(addedBy)}
+・${toBold("By")} : ${toBold(addedBy)}
 ・${toBold("Made Admin")} : ${toBold(newAdmin)}
-・${toBold("Time")}     : ${toBold(timeNow)}`;
+・${toBold("Time")} : ${toBold(timeNow)}`;
 
                         api.sendMessage(msg, threadID);
                     }
@@ -61,9 +61,9 @@ module.exports.run = async function ({ event, api, Threads, Users }) {
                         let removedAdmin = await Users.getNameUser(logMessageData.TARGET_ID);
 
                         const msg = `[ ADMIN REMOVED ]
-・${toBold("By")}        : ${toBold(removedBy)}
-・${toBold("Removed")}    : ${toBold(removedAdmin)}
-・${toBold("Time")}     : ${toBold(timeNow)}`;
+・${toBold("By")} : ${toBold(removedBy)}
+・${toBold("Removed")} : ${toBold(removedAdmin)}
+・${toBold("Time")} : ${toBold(timeNow)}`;
 
                         api.sendMessage(msg, threadID);
                     }
