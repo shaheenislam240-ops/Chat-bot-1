45a055dcfffbe1aeae026f0806fe7799a3d8a17f
rX Abdullah <rxmaria761@gmail.com>
2025-10-11 16:21:16 +0600

    Update antigali.js

--- Script/commands/antigali.js
+++ Script/commands/antigali.js
@@ -184,7 +184,7 @@ module.exports.run = async function ({ api, event, args }) {
   try {
     if (args[0] === "on") {
       antiGaliStatus = true;
-      return api.sendMessage("🎀 <\n 𝐀𝐧𝐭𝐢-𝐠𝐚𝐥𝐢 𝐢𝐬 𝐧𝐨𝐰 𝐨𝐧, event.threadID);
+      return api.sendMessage("🎀 <\n𝐀𝐧𝐭𝐢-𝐠𝐚𝐥𝐢 𝐢𝐬 𝐧𝐨𝐰 𝐨𝐧", event.threadID);
     } else if (args[0] === "off") {
       antiGaliStatus = false;
       return api.sendMessage("🎀 <\n𝐀𝐧𝐭𝐢-𝐆𝐚𝐥𝐢 𝐬𝐲𝐬𝐭𝐞𝐦 𝐢𝐬 𝐧𝐨𝐰 𝐎𝐅𝐅", event.threadID);
