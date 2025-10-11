1206e522e05212f042fefb73a1260ac951f4de16
rX Abdullah <rxmaria761@gmail.com>
2025-10-11 11:37:49 +0600

    Update 0gali.js

--- Script/commands/0gali.js
+++ Script/commands/0gali.js
@@ -1,26 +1,45 @@
 const fs = require("fs");
 module.exports.config = {
 	name: "gali",
-    version: "1.0.1",
+    version: "1.0.4",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", //Modify by rX
+	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Don't Change Credits",
 	commandCategory: "no prefix",
-	usages: "mgi",
+	usages: "mgi|suar",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	if (event.body.indexOf("Mgi")==0 || event.body.indexOf("Khanki")==0 || event.body.indexOf("Mg")==0 || event.body.indexOf("Magi")==0) {
-		var msg = {
-				body: "aisob kintu vlo lge na",
-				attachment: fs.createReadStream(__dirname + `/noprefix/nupure.mp3`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("😡", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID, body } = event;
 
-  }
+	// Original gali keywords
+	const galiKeywords = ["Mgi","Khanki","Mg","Magi"];
+
+	// Suar keywords (both trigger same audio)
+	const suarKeywords = ["Suar", "Suar er bacca"];
+
+	// Check for gali keywords
+	if (galiKeywords.some(keyword => body.indexOf(keyword) == 0)) {
+		var msg = {
+			body: "aisob kintu vlo lge na",
+			attachment: fs.createReadStream(__dirname + `/noprefix/nupure.mp3`)
+		}
+		api.sendMessage(msg, threadID, messageID);
+		api.setMessageReaction("😡", messageID, (err) => {}, true);
+	}
+
+	// Check for suar keywords
+	if (suarKeywords.some(keyword => body.indexOf(keyword) == 0)) {
+		var msg = {
+			body: "Suar kotha bolle bhalo lage na",
+			attachment: fs.createReadStream(__dirname + `/noprefix/suar.mp3`)
+		}
+		api.sendMessage(msg, threadID, messageID);
+		api.setMessageReaction("😡", messageID, (err) => {}, true);
+	}
+}
+
+module.exports.run = function({ api, event, client, __GLOBAL }) {
+  // Empty run function
+	}
