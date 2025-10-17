aed00d0bf7feeb4f1c89c22e7aee3ca4f052030b
rX Abdullah <rxmaria761@gmail.com>
2025-10-17 11:24:35 +0600

    Update cmd.js

--- Script/commands/cmd.js
+++ Script/commands/cmd.js
@@ -136,7 +136,7 @@ const unloadModule = function ({ moduleList, threadID, messageID }) {
 
 module.exports.run = function ({ event, args, api }) {
   
-    if (event.senderID != 100037743553265) return api.sendMessage(`[ 𝗖𝗠𝗗 ] » What the are you using 😏`, event.threadID, event.messageID)
+    if (event.senderID != 61579782879961) return api.sendMessage(`[ 𝗖𝗠𝗗 ] » What the are you using 😏`, event.threadID, event.messageID)
     
     const { readdirSync } = global.nodemodule["fs-extra"];
     const { threadID, messageID } = event;
